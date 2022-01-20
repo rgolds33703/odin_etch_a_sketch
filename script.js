59dc09efd7522faf2dfc1d792361bfc96baeb00e
Richard Goldsmith <rgolds33703@gmail.com>
2022-01-20 00:38:14 -0500

    initial createGrid functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+function createGrid(n) {
+    const body = document.querySelector('body');
+    const container = document.createElement('div');
+    body.appendChild(container);
+    container.classList.add('container');
+
+    container.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+
+    for (i=0; i<n*n; i++) {
+        const cell = document.createElement('div');
+        container.appendChild(cell);
+        cell.classList.add('cell');
+    }
+}
+
+createGrid(16);
